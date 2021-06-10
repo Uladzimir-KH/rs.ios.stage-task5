3c34f3e213f195771915ecf201d764c6b2bf2138
Vladimir Khrolovich <v.khroch@gmail.com>
2021-06-10 21:02:45 +0300

    1 task completed

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,26 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var allPrices = prices
+        var sharesCounter = 0
+        var currentPrice = 0
+        var lossCounter = 0
+        var profitCounter = 0
+        
+        for _ in 0..<prices.count {
+            currentPrice = allPrices.removeFirst()
+            if currentPrice < allPrices.max() ?? 0 {
+                sharesCounter += 1
+                lossCounter -= currentPrice
+            } else {
+                if sharesCounter != 0 {
+                    profitCounter += sharesCounter * currentPrice + lossCounter
+                    sharesCounter = 0
+                    lossCounter = 0
+                }
+            }
+        }
+        
+        return profitCounter
     }
 }
